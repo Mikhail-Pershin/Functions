2fb2c79e9ebf0ddd30e42fa88eb4398d55d95ec2
Mikhail_Pershin <mcpershin@yandex.ru>
2023-03-23 23:16:51 +0300

    add solution of task 29

--- Program.cs
+++ Program.cs
@@ -37,38 +37,62 @@
 // 9012 -> 12
 
 
-int number = ReadInt("Введите число: ");
-int len = NumberLen(number);
-SumNumbers(number, len);
+// int number = ReadInt("Введите число: ");
+// int len = NumberLen(number);
+// SumNumbers(number, len);
+
+
+// // Функция ввода
+// int ReadInt(string message)
+// {
+//   Console.Write(message);
+//   return Convert.ToInt32(Console.ReadLine());
+// }
+
+// // Функция подсчета цифр в числе
+// int NumberLen(int a)
+// {
+//   int index = 0;
+//   while (a > 0)
+//   {
+//     a /= 10;
+//     index++;
+//   }
+//   return index;
+// }
+
+// // Функция вывода суммы цифр в числе
+// void SumNumbers(int n, int len)
+// {
+//   int sum = 0;
+//   for (int i = 1; i <= len; i++)
+//   {
+//     sum += n % 10;
+//     n /= 10;
+//   }
+//   Console.WriteLine($"Сумма чисел в цифре -> {sum}");
+// }
+
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. (числа берете любые)
+
+int lenArray = ReadInt("Введите длинну массива: ");
+
+int[] randomArray = new int[lenArray];
+System.Console.Write("[ ");
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(10,100);
+    Console.Write($"{randomArray[i]} ");
+}
+System.Console.Write("]");
+
 
 
 // Функция ввода
 int ReadInt(string message)
 {
-  Console.Write(message);
-  return Convert.ToInt32(Console.ReadLine());
-}
-
-// Функция подсчета цифр в числе
-int NumberLen(int a)
-{
-  int index = 0;
-  while (a > 0)
-  {
-    a /= 10;
-    index++;
-  }
-  return index;
-}
-
-// Функция вывода суммы цифр в числе
-void SumNumbers(int n, int len)
-{
-  int sum = 0;
-  for (int i = 1; i <= len; i++)
-  {
-    sum += n % 10;
-    n /= 10;
-  }
-  Console.WriteLine($"Сумма чисел в цифре -> {sum}");
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
 }
